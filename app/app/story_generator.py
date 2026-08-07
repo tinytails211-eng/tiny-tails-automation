e6172c8d1d5da65c53334927477511f78e814117
tinytails211-eng <tinytails211@gmail.com>
2026-08-07 00:45:11 -0700

    Add story generator

---
+++ app/app/story_generator.py
@@ -0,0 +1,9 @@
+def create_story():
+    return {
+        "title": "The Puppy and the Lost Duckling",
+        "scenes": [
+            "A puppy walks through a colorful forest.",
+            "The puppy finds a little duckling.",
+            "They become friends and explore together."
+        ]
+    }
